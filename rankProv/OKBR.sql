0d5d2a3e91c50219567dd37cb7040ca3dab55bb3
mujji89 <mujtaba.rafeeq@gmail.com>
2016-06-29 15:49:05 +0200

    OKBR SQL and Pom

---
+++ rankProv/OKBR.sql
@@ -0,0 +1,31 @@
+/*
+SQLyog Ultimate v9.10 
+MySQL - 5.6.21 : Database - okbr
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*Table structure for table `truthfinder` */
+
+DROP TABLE IF EXISTS `truthfinder`;
+
+CREATE TABLE `truthfinder` (
+  `id` int(10) unsigned NOT NULL,
+  `sourceId` int(10) unsigned NOT NULL,
+  `factId` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `truthfinder` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
