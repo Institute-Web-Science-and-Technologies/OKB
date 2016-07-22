a50a97cb6dd5054e8c2f12d5de6d5c50990a64d8
Alex Baier <abaier@uni-koblenz.de>
2016-07-22 22:50:13 +0100

    Add UNIQUE property to all primary keys. Added propertyid to Qualifiers.

--- okbc-rest/sql_setup/01 create_table.sql
+++ okbc-rest/sql_setup/01 create_table.sql
@@ -1,12 +1,12 @@
 CREATE TABLE Events (
   -- Corresponds directly to its Wikidata equivalent.
-  eventid  INT PRIMARY KEY,
+  eventid  INT PRIMARY KEY UNIQUE,
   label    NVARCHAR(255),
   lastedited DATETIME NOT NULL
 );
 
 CREATE TABLE Statements (
-  statementid INT PRIMARY KEY AUTO_INCREMENT,
+  statementid INT PRIMARY KEY AUTO_INCREMENT UNIQUE,
   propertyid INT NOT NULL,
   label      NVARCHAR(255),
   datatype   NVARCHAR(255) NOT NULL,
@@ -15,13 +15,13 @@ CREATE TABLE Statements (
 );
 
 CREATE TABLE Users (
-  userid INT PRIMARY KEY AUTO_INCREMENT,
+  userid INT PRIMARY KEY AUTO_INCREMENT UNIQUE,
   username VARCHAR(255) UNIQUE NOT NULL ,
   reputation FLOAT NOT NULL
 );
 
 CREATE TABLE Claims (
-  claimid    INT PRIMARY KEY AUTO_INCREMENT,
+  claimid    INT PRIMARY KEY AUTO_INCREMENT UNIQUE,
   snaktype   ENUM ('value', 'novalue', 'missingvalue'),
   cvalue     NVARCHAR(255),
   ranking    ENUM ('deprecated', 'normal', 'preferred'),
@@ -32,7 +32,7 @@ CREATE TABLE Claims (
 );
 
 CREATE TABLE Refs (
-  refid            INT PRIMARY KEY AUTO_INCREMENT,
+  refid            INT PRIMARY KEY AUTO_INCREMENT UNIQUE,
   url              NVARCHAR(255),
   title            NVARCHAR(255),
   publicationdate  DATE,
@@ -45,14 +45,15 @@ CREATE TABLE Refs (
 );
 
 CREATE TABLE Authors (
-  authorid    INT PRIMARY KEY AUTO_INCREMENT,
+  authorid    INT PRIMARY KEY AUTO_INCREMENT UNIQUE,
   author NVARCHAR(255),
   refid  INT,
   FOREIGN KEY (refid) REFERENCES Refs(refid)
 );
 
 CREATE TABLE Qualifiers (
-  qualifierid INT PRIMARY KEY AUTO_INCREMENT,
+  qualifierid INT PRIMARY KEY AUTO_INCREMENT UNIQUE,
+  propertyid INT NOT NULL,
   datatype   ENUM ('quantity', 'item', 'property', 'url', 'time', 'globe coordinate', 'string'),
   qvalue     NVARCHAR(255) NOT NULL,
   claimid    INT NOT NULL,
@@ -60,7 +61,7 @@ CREATE TABLE Qualifiers (
 );
 
 CREATE TABLE Categories (
-  ctid     INT PRIMARY KEY AUTO_INCREMENT,
+  ctid     INT PRIMARY KEY AUTO_INCREMENT UNIQUE,
   category NVARCHAR(255),
   eventid  INT,
   FOREIGN KEY (eventid) REFERENCES Events(eventid)
