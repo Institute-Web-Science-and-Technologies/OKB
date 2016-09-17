324a83d426cfb2fb72c06dcd90b53d8c66b8b378
codezzmaven <coezzmaven@gmail.com>
2016-09-17 07:45:53 +0200

    Initial commit for truthfinder Matrix initalised and implications added. TODO: read data from tables. TODO: Test the application. TODO: calculate precision for this algo.

--- rankProv/src/main/java/algos/TruthFinder.java
+++ rankProv/src/main/java/algos/TruthFinder.java
@@ -1,15 +1,13 @@
 package algos;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.io.ObjectStreamClass;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.sql.Timestamp;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import cern.colt.matrix.DoubleFactory1D;
 import cern.colt.matrix.DoubleMatrix1D;
@@ -17,98 +15,147 @@ import cern.colt.matrix.impl.SparseDoubleMatrix1D;
 import cern.colt.matrix.impl.SparseDoubleMatrix2D;
 import cern.jet.math.Functions;
 
-
-/** TruthFinder Algorithm
- * @author OKB-R
- * Takes a set of facts provided by set of websites and computes trustworthiness 
+/**
+ * TruthFinder Algorithm
+ * 
+ * @author OKB-R Takes a set of facts provided by set of websites and computes
+ *         trustworthiness
  */
 public class TruthFinder {
+	Map <Integer, Integer> matrixIdToClaimIdMap;
+	Map <Integer, Integer> claimIdToMatrixIdMap;
+	Map <Integer, String> matrixIdToSourceMap;
+	Map <String, Integer> sourceToMatrixIdMap;
+	Map <String, List<Integer>> sourceClaimIdListMap;
+	Map <Integer, Map<Integer, String>> eventIdClaimIdPublicationDateMap;
+	Map <Integer, Double> claimImplications;
 	SparseDoubleMatrix2D A, B;
 	double initTrust = 0.2;
 	double dampeningFactor = 0.3;
 	DoubleMatrix1D trustVector, logTrustVector, confidenceVector, logConfidenceVector, temp;
-	String aMatrix = "amatrix";
-	String bMatrix = "bmatrix";
-	String trustVectorObject = "trustVectorObject";
 
-	public TruthFinder(double value[][]) {
-		B = new SparseDoubleMatrix2D(value);// this will change after
+	public TruthFinder(Map <String, List<Integer>> sourceClaimIdListMap, 
+			Map <Integer, Map<Integer, String>> eventIdClaimIdPublicationDateMap) {
 		// implication
-		A = new SparseDoubleMatrix2D(B.columns(), B.rows());
+		this.sourceClaimIdListMap = sourceClaimIdListMap;
+		this.eventIdClaimIdPublicationDateMap = eventIdClaimIdPublicationDateMap;
 		this.setA();
 		this.initilaizeTrustVectors();
 
-		// confidenceVector = new SparseDoubleMatrix1D(arg0)
+		// confidenceVector = new SparseDoubleMatrix1D(arg0)		
 	}
+   public void initialiseA(){
+	   int sourceMatrixId = 0;
+	   int claimMatrixId = 0;
+	   this.matrixIdToClaimIdMap = new HashMap <Integer, Integer> ();
+	   this.claimIdToMatrixIdMap  = new HashMap <Integer, Integer> ();
+	   this.matrixIdToSourceMap  = new HashMap <Integer, String> ();
+	   this.sourceToMatrixIdMap = new HashMap <String, Integer> ();
+	   String sourceName;
+	   List<Integer> claimsIdList;
+	   for (Map.Entry<String, List<Integer>> entry : this.sourceClaimIdListMap.entrySet()) {
+		   sourceName = entry.getKey();
+		   this.matrixIdToSourceMap.put(sourceMatrixId, sourceName);
+		   this.sourceToMatrixIdMap.put(sourceName, sourceMatrixId);
+		   claimsIdList = entry.getValue();
+		   sourceMatrixId++;
+		   for( int claimId : claimsIdList) {
+			   if(! this.claimIdToMatrixIdMap.containsKey( claimId) ) {
+				   this.claimIdToMatrixIdMap.put(claimId, claimMatrixId);
+				   this.matrixIdToClaimIdMap.put(claimMatrixId, claimId);
+				   claimMatrixId++;
+			   }
+		   }
+	   }
+	   int totalNoOfSources = sourceMatrixId++;
+	   int totalNoOfClaims = claimMatrixId++;
+	   A = new SparseDoubleMatrix2D(totalNoOfSources , totalNoOfClaims );
+	   
+		  
+   }
+   public void initialiseB(){
+	   B = new SparseDoubleMatrix2D(A.columns(), A.rows());
+   }
+   public long stringToTimeStamp(String dateString){
+	   try {
+		      DateFormat formatter;
+		      formatter = new SimpleDateFormat("dd/MM/yyyy");
+		      Date date = (Date) formatter.parse(dateString);
+		      long timeStampDate = date.getTime();
 
+		      return timeStampDate;
+		    } catch (ParseException e) {
+		      System.out.println("Exception :" + e);
+		      return 0l;
+		    }
+	   
+   }
+   public void calculateImplication(){
+	   for(Map.Entry<Integer, String> entry : this.matrixIdToSourceMap.entrySet()){
+		   this.claimImplications.put(entry.getKey(), 1.0);
+	   }
+	   Map<Integer, String> claimsOfSameEvent;
+	   String publicationDate;
+	   int claimId;
+	   long timestamp1;
+	   long timestamp2;
+	   long closeness = 0;
+	   double implication = 1.0;
+	   for(Map.Entry<Integer, Map<Integer, String>> entry: this.eventIdClaimIdPublicationDateMap.entrySet()){
+		   claimsOfSameEvent = entry.getValue();
+		   for(Map.Entry<Integer, String> claimsEntry : claimsOfSameEvent.entrySet()){
+			  
+			   publicationDate = claimsEntry.getValue();
+			   claimId =claimsEntry.getKey();
+			  timestamp1 = stringToTimeStamp(publicationDate);
+			  for(Map.Entry<Integer, String> claimsEntry1 : claimsOfSameEvent.entrySet()){
+				 timestamp2 =  stringToTimeStamp(claimsEntry1.getValue());
+				 closeness += Math.abs( timestamp2 - timestamp1);
+			  }
+			  implication = Math.exp( ( -1.0 * closeness));
+			  this.claimImplications.put(claimId, implication);
+		   }
+	   }
+   }
 	public void setA() {
-		Path p = Paths.get(aMatrix);
-		FileInputStream in;
-		ObjectInputStream ins;
-		if(Files.exists(p)){
-			try {
-				in = new FileInputStream(aMatrix); 
-				ins = new ObjectInputStream(in);
-				A = (SparseDoubleMatrix2D) ins.readObject();
-			} catch (FileNotFoundException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (ClassNotFoundException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-		else{
-			Double sum = 0.0;
-			DoubleMatrix1D col, row;
-			for (int i = 0; i < B.columns(); i++) {
-				col = this.B.viewColumn(i);
-				sum = col.zSum();
-				sum = 1 / sum;
-				row = A.viewRow(i);
-				// row = row./sum;
-				for (int j = 0; j < row.size(); j++) {
-					if (col.get(j) > 0) {
-						A.setQuick(i, j, sum);
-					}
-				}
-			}
-		}
+		String sourceName;
+		List<Integer> claimsIdList;
+		int numberOfClaimsSupportedBySource;
+		for (Map.Entry<String, List<Integer>> entry : this.sourceClaimIdListMap.entrySet()) {
+			   sourceName = entry.getKey();
+			   claimsIdList = entry.getValue();
+			   numberOfClaimsSupportedBySource = claimsIdList.size();
+			   for( int claimId : claimsIdList) {
+				   A.setQuick(this.sourceToMatrixIdMap.get(sourceName), 
+						   this.claimIdToMatrixIdMap.get(claimId), 1.0/numberOfClaimsSupportedBySource);
+			   }
+		   }
 	}
 
+	public void setB(){
+		String sourceName;
+		List<Integer> claimsIdList;
+		int numberOfClaimsSupportedBySource;
+		for (Map.Entry<String, List<Integer>> entry : this.sourceClaimIdListMap.entrySet()) {
+			   sourceName = entry.getKey();
+			   claimsIdList = entry.getValue();
+			   numberOfClaimsSupportedBySource = claimsIdList.size();
+			   if(numberOfClaimsSupportedBySource == 1)
+			   for( Integer claimId : claimsIdList) {
+				   B.setQuick(this.claimIdToMatrixIdMap.get(claimId), 
+						   this.sourceToMatrixIdMap.get(sourceName),
+						   this.claimImplications.get(claimId));
+			   }
+		   }
+	}
 	public void initilaizeTrustVectors() {
-		Path trustVectorObjectPath = Paths.get(trustVectorObject);
-		FileInputStream inputStream;
-		ObjectInputStream objectInputStream;
-		if(Files.exists(trustVectorObjectPath)){
-			try{
-				inputStream = new FileInputStream(trustVectorObject);
-				objectInputStream = new ObjectInputStream(inputStream);
-				trustVector = (SparseDoubleMatrix1D)objectInputStream.readObject();
-			}
-			catch (FileNotFoundException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (ClassNotFoundException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} 
-		}
-		else{
-			trustVector = new SparseDoubleMatrix1D(A.rows());
+		trustVector = new SparseDoubleMatrix1D(A.rows());
 
-
-			for (int i = 0; i < trustVector.size(); i++) {
-				trustVector.set(i, initTrust);
-				//logTrustVector.set(i, -1 * Math.log10(1 - initTrust));
-			}
+		for (int i = 0; i < trustVector.size(); i++) {
+			trustVector.set(i, initTrust);
+			// logTrustVector.set(i, -1 * Math.log10(1 - initTrust));
 		}
+		// }
 		logTrustVector = new SparseDoubleMatrix1D(trustVector.size());
 		confidenceVector = new SparseDoubleMatrix1D(B.rows());
 
@@ -116,11 +163,13 @@ public class TruthFinder {
 		logConfidenceVector = new SparseDoubleMatrix1D(B.rows());
 		convertTrustToLogTrust();
 	}
-	public void convertTrustToLogTrust(){
+
+	public void convertTrustToLogTrust() {
 		for (int i = 0; i < trustVector.size(); i++) {
 			logTrustVector.set(i, -1 * Math.log10((1 - trustVector.get(i))));
 		}
 	}
+
 	public void calculateConfidenceVectors() {
 
 		// logConfidenceVector = B*logTrustVector
@@ -132,7 +181,7 @@ public class TruthFinder {
 		// Now we compute s(confidence of a fact) from sigma using eq 8
 		// Computing sf(confidence of a fact) from
 		for (int i = 0; i < logConfidenceVector.size(); i++) {
-			double val = 1 / (1 + Math.exp((-dampeningFactor * logConfidenceVector.get(i)))); 
+			double val = 1 / (1 + Math.exp((-dampeningFactor * logConfidenceVector.get(i))));
 			// here we
 			// can
 			// try
@@ -161,32 +210,6 @@ public class TruthFinder {
 		double dotProduct = val / (val1 * val2);
 		// If similarity reaches to alpha
 		if (dotProduct > alpha) {
-			FileOutputStream out;
-			ObjectOutputStream os;
-			try {
-				System.out.println("Trust on Website");
-				System.out.println(trustVector);
-				System.out.println("\nConfidence on facts");
-				System.out.println(confidenceVector);
-				out = new FileOutputStream(aMatrix);
-				os = new ObjectOutputStream(out);
-				os.writeObject(A);
-				out = new FileOutputStream(bMatrix);
-				os = new ObjectOutputStream(out);
-				os.writeObject(A);
-				out = new FileOutputStream(trustVectorObject);
-				os = new ObjectOutputStream(out);
-				os.writeObject(trustVector);
-				os.close();
-				out.close();
-
-			} catch (FileNotFoundException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
 			return true;
 		} else {
 			System.out.println("Trust on Website");
@@ -197,8 +220,4 @@ public class TruthFinder {
 			return false;
 		}
 	}
-
-
-
 }
-
