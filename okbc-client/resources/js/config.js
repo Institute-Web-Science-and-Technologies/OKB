fa3ff8e4a999138d3838ebc673eb160fbb6be651
Alex Baier <abaier@uni-koblenz.de>
2016-09-18 16:03:32 +0100

    Changed config.

--- okbc-client/resources/js/config.js
+++ okbc-client/resources/js/config.js
@@ -1,3 +1,3 @@
-var CURATING_DATA_POST_URL = 'http://localhost:4567/addCuratedClaim';
-var OKB_EVENT_BASE_URL = 'http://localhost:4567';
+var CURATING_DATA_POST_URL = 'http://localhost:4567/OKBC-0.0.1-SNAPSHOT/addCuratedClaim';
+var OKB_EVENT_BASE_URL = 'http://localhost:4567/OKBC-0.0.1-SNAPSHOT';
 var MAX_NUM_SHOWN_ON_LEADERBOARD = 100;
